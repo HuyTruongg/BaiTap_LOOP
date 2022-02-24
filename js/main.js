76f27a727a621aa04049103859f8f6e7edffdc4d
Huy <truong.huy96@gmail.com>
2022-02-24 13:17:43 +0700

    bài 5

--- js/main.js
+++ js/main.js
@@ -157,8 +157,6 @@ function thayMau() {
     }
 } document.getElementById("btn6FOR").onclick = thayMau;
 
-function taoDiv(){for(var e="",t=1;t<=10;t++)e+=t%2==0?"<div class='bg-danger text-white p-2'>Div chẵn</div>":"<div class='bg-primary text-white  p-2'>Div lẻ </div>";document.getElementById("txtResult6").innerHTML=e}
-
 
 
 // In số nguyên tố
